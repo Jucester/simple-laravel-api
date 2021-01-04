50e4762c41b34a2905b15b8e68dc1aeb5fc59631
unknown <juceld@gmail.com>
2021-01-03 23:35:21 -0400

    Second

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Article;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +16,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/testing-api', function() {
-    return ['message' => 'Hi, user'];
-});
+// Simple API fort articles
+//  1. get all (GET) /api/articles
+//  2. create article (POST) /api/articles
+//  3. get a single article (GET) /api/article/{id}
+//  4. update an article (PUT/PATCH) /api/article/{id}
+//  5. delete an article (DELETE) /api/article/{id}
+
+
+Route::get('/articles', 'App\Http\Controllers\ArticleController@index');
+Route::get('/articles/{article}', 'App\Http\Controllers\ArticleController@index');
+
+Route::post('/articles', 'App\Http\Controllers\ArticleController@store');
+Route::put('/articles/{article}', 'App\Http\Controllers\ArticleController@index');
+Route::delete('/articles/{article}', 'App\Http\Controllers\ArticleController@index');
+
+
+
+
+
+
+// Steps:
+// 1. Create DB and Migrations
+// 2. 
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
